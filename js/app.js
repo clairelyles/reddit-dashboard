6a78cf3531a5da98b266ed1275eb2c6c7528174a
clairelyles <claire.lyles@gmail.com>
2015-02-04 11:44:02 -0800

    added modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,70 @@
-/* put js code here */
+var redditApp = angular.module("RedditApp",['ui.bootstrap'])
+  // alert('app loading')
 
-console.log('this file is running.');
+redditApp.controller('SearchController',['$scope','$http', '$modal', function($scope, $http, $modal) {
+  // alert('Controller loading');
+
+  $scope.results = {};
+  $scope.searchTerm = "";
+  $scope.allResults = JSON.parse(window.localStorage.allResults || "[]");
+
+
+  $scope.search = function() {
+
+  //error code goes here
+
+  var req = {
+      url: "http://www.reddit.com/search.json",
+      params: {
+        q: $scope.searchTerm
+      }
+    }
+
+  $http(req).success(function(data) {
+
+      $scope.results = data.data.children;
+      $scope.allResults.push($scope.searchTerm);
+      window.localStorage.allResults = JSON.stringify($scope.allResults);
+    })
+  }
+
+  $scope.load = function(idx){
+
+    var req = {
+      url: "http://www.reddit.com/search.json?",
+      params: {
+        q: $scope.allResults[idx]
+      }
+    }
+
+    $http(req).success(function(data){
+      $scope.results = data.data.children
+    })
+  }
+
+  $scope.clear = function() {
+    $scope.searchTerm = "";
+  }
+
+  $scope.delete = function(idx) {
+    $scope.allResults.splice(idx,1)
+    window.localStorage.allResults = JSON.stringify($scope.allResults);
+  }
+
+  $scope.showAbout = function() {
+    var aboutModal = $modal.open({
+      templateUrl:'aboutModal.html',
+      controller: 'AboutModalCtrl',
+      size: 'md'
+    });
+  }
+
+}]);
+
+redditApp.controller('AboutModalCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance){
+  $scope.greeting='Blaaaah';
+  $scope.closeThisModal = function() {
+    $modalInstance.close();
+  }
+}])
+
